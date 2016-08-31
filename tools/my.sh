4dacca82bc37f14ace37b583f19c5ad408342b70
xiongfusong <xiongfusong@gmail.com>
2016-08-31 10:33:57 +0800

    put the configure to the head

--- tools/my.sh
+++ tools/my.sh
@@ -4,9 +4,16 @@
 # sh my.sh db_xx2
 #########################################################################
 #!/bin/bash
+
+mysql=/usr/local/mysql/bin/mysql
+db_user=userxxx
+db_pwd=pwdxxx
 db_name=$1
+
 if [ -z $db_name ];then
   db_name=db_xx1
 fi
-/usr/local/mysql/bin/mysql -uuserxxx -ppwdxxx $db_name --default-character-set=utf8 --auto-rehash --prompt="[\d]>\_"
+$mysql -u${db_user} -p${db_pwd} $db_name --default-character-set=utf8 --auto-rehash --prompt="[\d]>\_"
+
+exit 0
 
